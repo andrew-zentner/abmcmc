c9748689457b30c647a77fe072b6f632437ee09f
Andrew Hearin <andrew.hearin@yale.edu>
2016-06-12 19:33:01 -0400

    Fixed auto-compile shell script. Should work now, @vdbosch69.

--- compile_script.sh
+++ compile_script.sh
@@ -1,8 +1,13 @@
 perl -pi -w -e 's/haloe/halo/g;' ms.tex
 perl -pi -w -e 's/colour/color/g;' ms.tex
-git clean -dfx
-pdflatex ms.tex
-bibtex ms.bib
-pdflatex ms.tex
-pdflatex ms.tex
-pdflatex ms.tex
+rm *.aux
+rm *.log
+rm *.synctex.gz
+rm *.bbl
+rm *.blg
+rm *.out
+pdflatex ms
+bibtex ms
+pdflatex ms
+pdflatex ms
+pdflatex ms
